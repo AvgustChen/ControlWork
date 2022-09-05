e86610d124ed9b7235776d361c930f22f76264bc
AvgustChen <avchen@yandex.ru>
2022-09-05 16:47:47 +0300

    Создан метод наполнения массива пользователем

--- Program.cs
+++ Program.cs
@@ -3,4 +3,26 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться
 // колекциями, лучше обойтись исключительно массивами.
 
+string[] MainArray (int size)
+{
+    string[] mainarray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i+1} строку: ");
+        mainarray[i] = Console.ReadLine();
+    }
+    return mainarray;
+}
+void ShowArray(string[] array)//показать массив
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
 
+    Console.WriteLine();
+}
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] mainarray = MainArray(size);
+
+ShowArray (mainarray);
